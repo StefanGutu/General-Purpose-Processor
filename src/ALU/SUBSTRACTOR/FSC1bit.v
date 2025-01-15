f19d11c1984a80ed0943c8b83bdbd0882140dfa0
Jayiana <164218155+Jayiana@users.noreply.github.com>
2025-01-16 00:42:58 +0200

    Delete src/ALU/SUBSTRACTOR/FSC1bit.v

--- src/ALU/SUBSTRACTOR/FSC1bit.v
+++
@@ -1,7 +0,0 @@
-module FSC1bit(a, b, bin, diff, bout);
-    input a, b, bin;   
-    output diff, bout;     
-
-    assign diff = a ^ b ^ bin;
-    assign bout = (~a & b) | ((~a | b) & bin);
-endmodule
